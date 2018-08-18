693ea32852a39015e099ba306c2d6bdb158c311f
Eduardo Bonamigo <edbmuller@gmail.com>
2018-08-18 16:33:57 -0300

    Aula 1 - Parametros construtor e Metodo

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,7 +1,11 @@
 class Negociacao {
-    constructor() {
-        this.data = new Date();
-        this.quantidade = 2;
-        this.valor = 10;
+    constructor(data, quantidade, valor) {
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    getVolume() {
+        return this.quantidade * this.valor;
     }
 }
