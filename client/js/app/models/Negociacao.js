40ce9baeaed9faf15110b876ba683efdebf58835
Eduardo Bonamigo <edbmuller@gmail.com>
2018-08-18 21:05:56 -0300

    Aula 2.4 / 2.5 - Encapsulamento / Sintaxe get

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,11 +1,25 @@
 class Negociacao {
+    //Declaração das propriedades 
     constructor(data, quantidade, valor) {
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
+    
+    //Getters das propriedades
+    get data() {
+        return this._data;
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+    
+    /*** Metodos: ***/
 
     getVolume() {
-        return this.quantidade * this.valor;
+        return this._quantidade * this._valor;
     }
 }
