1962fade0fdabc1cad6c0e47f4d058521ff90162
Eduardo Bonamigo <edbmuller@gmail.com>
2018-10-03 21:58:07 -0300

    Modelo ListaNegociacoes

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,23 +4,36 @@ class NegociacaoController {
     
     var $ = document.querySelector.bind(document);
   
+    //propriedades de interação
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');    
-    
+    this._listaNegociacoes = new ListaNegociacoes();
   }
 
+  //adiciona negociação
   adiciona(event) {
     event.preventDefault();
 
-    let negociacao = new Negociacao(
+    // cria a negociação utilizando o modelo negociacoes
+    this._listaNegociacoes.adiciona(this._criaNegociacao());
+    this._limpaFormulario();  
+    console.log(this._listaNegociacoes.negociacoes);
+  } 
+
+  _criaNegociacao() {
+    return new Negociacao(
       DataHelper.textoParaData(this._inputData.value),
       this._inputQuantidade.value,
       this._inputValor.value
-    )
+    );  
+  }
 
-    let diaMesAno = DataHelper.dataParaTexto(negociacao.data);
-    console.log(diaMesAno);
-
+  _limpaFormulario() {
+    this._inputData.value = "";
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0;
+    
+    this._inputData.focus() ;
   }
 }
